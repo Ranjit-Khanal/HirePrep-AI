843cd87fcaa2513c550d95f488d1b595caa3daac
Ranjit-Khanal <ranjitkhanal61@gmail.com>
2026-03-01 14:37:30 +0545

    Add navigation button to return to dashboard in Interview component

--- src/features/interview/pages/Interview.jsx
+++ src/features/interview/pages/Interview.jsx
@@ -61,6 +61,7 @@ const Interview = () => {
     const [ activeNav, setActiveNav ] = useState('technical')
     const { report, getReportById, loading, getResumePdf } = useInterview()
     const { interviewId } = useParams()
+    const navigate = useNavigate();
 
     useEffect(() => {
         if (interviewId) {
@@ -185,6 +186,13 @@ const Interview = () => {
                             ))}
                         </div>
                     </div>
+
+                     <button
+                        onClick={() => navigate('/')}
+                        className='button primary-button' >
+                      <svg  height={"0.8rem"} style={{ marginRight: "0.8rem" }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"   stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-step-back-icon lucide-step-back"><path d="M13.971 4.285A2 2 0 0 1 17 6v12a2 2 0 0 1-3.029 1.715l-9.997-5.998a2 2 0 0 1-.003-3.432z"/><path d="M21 20V4"/></svg>
+                        Back to Dashboard
+                    </button>
 
                 </aside>
             </div>
